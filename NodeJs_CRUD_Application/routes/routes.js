fa5fda76352cf520922e6570f3d8e9d0440480a4
Amarjeet Kumar <amarjeetkumar802212@gmail.com>
2022-12-28 02:29:04 +0530

    Cover front-end design

--- NodeJs_CRUD_Application/routes/routes.js
+++ NodeJs_CRUD_Application/routes/routes.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/users", (req, res) => {
-  res.send("All Users");
+router.get("/", (req, res) => {
+  res.render("index", { title: "Home Page" });
 });
+
+
+//  Creating Add Users route
+router.get("/add", (req, res) => {
+  res.render("add_users", { title: "Add Users" });
+});
+
+
 
 module.exports = router;
